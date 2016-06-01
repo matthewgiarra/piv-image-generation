a665a07aeade81d0a8b51a97c8a6f3454e4a345c
Matthew Giarra <matthew.giarra@gmail.com>
2016-06-01 17:21:59 -0400

    Change main.c to count the length of the base name and allocate memory so that the code doesn't seg-fault for long file paths.

--- c_codes/main.c
+++ c_codes/main.c
@@ -2,14 +2,17 @@
 #include <stdlib.h>
 #include <omp.h>
 #include <math.h>
+#include <string.h>
 #include "piv-image-gen.h"
 #include "image_writing.h"
 
 // Main function
 int main(int argc, char *argv[]){
 
-	if(argc < 6){
-		printf("Usage: ./main <num_rows> <num_cols> <particles_per_pix> <num_threads> <output_path>\n");
+	if(argc < 7){
+		printf("Usage: ./main <num_rows> <num_cols>" 
+			" <particles_per_pix> <num_threads>"
+				" <num_images> <output_base>\n");
 		return(-1);
 	}
 	
@@ -18,19 +21,34 @@ int main(int argc, char *argv[]){
 	const int num_cols = atoi(argv[2]);
 	float particles_per_pixel = atof(argv[3]);
 	int max_num_threads = atoi(argv[4]);
-	char *output_path = argv[5];
+	int num_images = atoi(argv[5]);
+	char *output_file_base = argv[6];
+	
+	// Length of the input string
+	int len = strlen(argv[6]);
+	
+	// Output file name stuff
+	char *output_file_path;
+	output_file_path = malloc((len + 20) * sizeof(char));
 	
 	// Number of particles
 	int num_particles;
 		
 	// Allocate the particle coordinates
-	float *X, *Y, *particle_diameters, *particle_max_intensities, *output_image;
+	float *X, *Y;
+	float *particle_diameters, *particle_max_intensities;
+	
+	// Allocate the output image
+	float *output_image;
+	
+	// Allocate the uint16 output image
+	uint16_t *output_image_uint16;
 	
 	// Number of pixels in the image
 	int num_pixels = num_rows * num_cols;
 		
 	// Counter	
-	int p;
+	int p, n;
 	
 	// Number of particles
 	num_particles = (int)round((float)num_pixels * (float)particles_per_pixel);
@@ -44,8 +62,12 @@ int main(int argc, char *argv[]){
 	float lower_bound_y = 0;
 	float upper_bound_y = num_rows;
 	
-	float lower_bound_dp = 1.8;
-	float upper_bound_dp = 4.40;
+	float lower_bound_dp = 2.8;
+	float upper_bound_dp = 8.8;
+	
+	// Max intensity of any particle.
+	float upper_bound_IO = pow(2, 15) - 1;
+	float lower_bound_IO = 0.1 * upper_bound_IO;
 	
 	// Allocate the array
 	X = malloc(num_particles * sizeof(float));
@@ -55,33 +77,53 @@ int main(int argc, char *argv[]){
 
 	// Allocate the output image
 	output_image = calloc(num_pixels, sizeof(float));
+	output_image_uint16 = malloc(num_pixels * sizeof(uint16_t));
 	
-	// Populate the arrays
-	devrand(X, num_particles, lower_bound_x, upper_bound_x);
-	devrand(Y, num_particles, lower_bound_y, upper_bound_y);
-	devrand(particle_diameters, num_particles, lower_bound_dp, upper_bound_dp);
+	// Print number of images
+	printf("Number of images: %d\n", num_images);
 	
-	// Particle diameters
-	for(p = 0; p < num_particles; p++){
-		particle_max_intensities[p] = lower_bound_dp / particle_diameters[p];
+	// Loop over images
+	for(n = 0; n < num_images; n++){
+		
+		sprintf(output_file_path, "%s%05d.tiff", output_file_base, n);
+		// printf("%s\n", output_file_path);
+		
+		// Populate the arrays
+		devrand(X, num_particles, lower_bound_x, upper_bound_x);
+		devrand(Y, num_particles, lower_bound_y, upper_bound_y);
+		devrand(particle_max_intensities, num_particles, lower_bound_IO, upper_bound_IO);
+		devrand(particle_diameters, num_particles, lower_bound_dp, upper_bound_dp);
+
+		// // Particle diameters
+		// for(p = 0; p < num_particles; p++){
+		// 	particle_max_intensities[p] = IO_max * lower_bound_dp / particle_diameters[p];
+		// }
+
+		// Generate the image
+		generateParticleImage_omp(output_image, num_rows,
+			num_cols, num_particles,
+			X, Y, particle_diameters,
+			particle_max_intensities, max_num_threads);
+
+		// Convert to uint16
+		for(p = 0; p < num_pixels; p++){
+			output_image_uint16[p] = (uint16_t)output_image[p];
+		}
+
+		// Write the image
+		// printf("Image save path: %s\n", output_file_path);
+		writeTiff_bw16(output_file_path, output_image_uint16 , num_rows, num_cols);
+	
 	}
 
-	// Generate the image
-	generateParticleImage_omp(output_image, num_rows, 
-		num_cols, num_particles, 
-		X, Y, particle_diameters, 
-		particle_max_intensities, max_num_threads);
-		
-	// Write the image
-	printf("Image save path: %s\n", output_path);
-			
-	
 	// Free the array
 	free(X);
 	free(Y);
 	free(output_image);
+	free(output_image_uint16);
 	free(particle_diameters);
 	free(particle_max_intensities);
+	free(output_file_path);
 	return(0);
 }
 
