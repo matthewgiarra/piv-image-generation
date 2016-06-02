45b556e86860a7824f91cf7c3e5e5c92f8790987
Matthew Giarra <matthew.giarra@gmail.com>
2016-06-01 22:47:04 -0400

    Added code to generate single micro PIV images, written in C.

--- c_codes/main.c
+++ c_codes/main.c
@@ -9,9 +9,8 @@
 // Main function
 int main(int argc, char *argv[]){
 
-	if(argc < 7){
+	if(argc < 6){
 		printf("Usage: ./main <num_rows> <num_cols>" 
-			" <particles_per_pix>"
 				" <num_images> <num_threads> <output_base>\n");
 		return(-1);
 	}
@@ -19,20 +18,18 @@ int main(int argc, char *argv[]){
 	// Command line arguments
 	const int num_rows = atoi(argv[1]);
 	const int num_cols = atoi(argv[2]);
-	float particles_per_pixel = atof(argv[3]);
-	int num_images = atoi(argv[4]);
-	int max_num_threads = atoi(argv[5]);
-	char *output_file_base = argv[6];
+	int num_images = atoi(argv[3]);
+	int max_num_threads = atoi(argv[4]);
+	char *output_file_base = argv[5];
 	
 	// Length of the input string
-	int len = strlen(argv[6]);
+	int len = strlen(argv[5]);
 		
 	// Number of particles
 	int num_particles;
 		
 	// Allocate the particle coordinates
-	float *X, *Y;
-	float *particle_diameters, *particle_max_intensities;
+	float *X, *Y, *Z;
 	
 	// Allocate the output image
 	float *output_image;
@@ -49,12 +46,29 @@ int main(int argc, char *argv[]){
 	// Counter	
 	int p, n;
 	
-	// Define this here for now
-	// Largest in focus particle diameter in pixels
-	float dp_max = 40.0;
+	// Some constants
+	float particle_diameter_microns = 0.1;
+	float objective_magnification   = 60;
+	float channel_depth_microns     = 100;
+	float wavelength_microns        = 0.532;
+	float pixel_size_microns 		= 10;
+	float particle_volume_fraction  = 5E-5;
+	float NA = 1.4;
+	float working_distance_microns  = 0.3E3;
+	float focal_length_microns      = 3.3333E3;
+	float int_max = 0.95 * pow(2, 16) - 1;
+	
+	// Size of the domain
+	float domain_width_microns  = (float)num_cols * pixel_size_microns / objective_magnification;
+	float domain_height_microns = (float)num_rows * pixel_size_microns / objective_magnification;
+	float domain_volume = domain_width_microns * domain_height_microns * channel_depth_microns;
+	
+	// Particle concentration in particles per cubic micron
+	float particle_concentration = 6 * particle_volume_fraction / 
+	    (PI * pow(particle_diameter_microns, 3));
 	
 	// Number of particles
-	num_particles = (int)round((float)num_pixels * (float)particles_per_pixel);
+	num_particles = (int)round(particle_concentration * domain_volume);
 	
 	// Number of particles
 	printf("Number of particles: %d\n", num_particles);
@@ -64,28 +78,22 @@ int main(int argc, char *argv[]){
 	float upper_bound_x = num_cols;
 	float lower_bound_y = 0;
 	float upper_bound_y = num_rows;
-	
-	float lower_bound_dp = 2.8;
-	float upper_bound_dp = 8.8;
-	
-	// Max intensity of any particle.
-	float upper_bound_IO = pow(2, 15) - 1;
-	float lower_bound_IO = 0.1 * upper_bound_IO;
-	
+	float lower_bound_z = -1 * channel_depth_microns / 2.0;
+	float upper_bound_z =  1 * channel_depth_microns / 2.0;
+
 	// Print number of images
 	printf("Number of images: %d\n", num_images);
 	
 	// Seed the random number generator
 	seed_random_number_generator();
 	
-	#pragma omp parallel private(X, Y, particle_max_intensities, particle_diameters, output_image, output_image_uint16, output_file_path) num_threads(max_num_threads)
-	{
+	// #pragma omp parallel private(X, Y, Z, output_image, output_image_uint16, output_file_path) num_threads(max_num_threads)
+	// {
 	
 		// Allocate the array
 		X = malloc(num_particles * sizeof(float));
 		Y = malloc(num_particles * sizeof(float));
-		particle_diameters = malloc(num_particles * sizeof(float));
-		particle_max_intensities = malloc(num_particles * sizeof(float));
+		Z = malloc(num_particles * sizeof(float));
 
 		// Allocate the output image
 		output_image = calloc(num_pixels, sizeof(float));
@@ -95,7 +103,7 @@ int main(int argc, char *argv[]){
 		// Output file name stuff
 		output_file_path = malloc((len + 20) * sizeof(char));
 	
-		#pragma omp for
+		// #pragma omp for
 		// Loop over images
 		for(n = 0; n < num_images; n++){
 	
@@ -105,20 +113,21 @@ int main(int argc, char *argv[]){
 			// Populate the arrays
 			devrand(X, num_particles, lower_bound_x, upper_bound_x);
 			devrand(Y, num_particles, lower_bound_y, upper_bound_y);
-			devrand(particle_max_intensities, num_particles, lower_bound_IO, upper_bound_IO);
-			devrand(particle_diameters, num_particles, lower_bound_dp, upper_bound_dp);
-
-			// // Particle diameters
-			// for(p = 0; p < num_particles; p++){
-			// 	particle_max_intensities[p] = IO_max * lower_bound_dp / particle_diameters[p];
-			// }
-
-			// Generate the image
-			generateParticleImage(output_image, num_rows,
-				num_cols, num_particles,
-				X, Y, particle_diameters,
-				particle_max_intensities, dp_max);
-
+			devrand(Z, num_particles, lower_bound_z, upper_bound_z);
+			
+			// Generate the micro PIV image.
+			generate_micro_piv_image(output_image, X, Y, Z, num_particles,
+											num_rows, num_cols, particle_diameter_microns, 
+											pixel_size_microns, particle_volume_fraction, 
+											channel_depth_microns, objective_magnification,
+											NA, working_distance_microns, focal_length_microns,
+											wavelength_microns);
+											
+			// Pixel gain
+			for(p = 0; p < num_pixels; p++){
+				output_image[p] = int_max * output_image[p];
+			}
+			
 			// Convert to uint16
 			for(p = 0; p < num_pixels; p++){
 				output_image_uint16[p] = (uint16_t)output_image[p];
@@ -135,14 +144,9 @@ int main(int argc, char *argv[]){
 		free(Y);
 		free(output_image);
 		free(output_image_uint16);
-		free(particle_diameters);
-		free(particle_max_intensities);
 		free(output_file_path);	
-		
-	}
+	// }
 	
-
-
 	return(0);
 }
 
--- c_codes/piv-image-gen.h
+++ c_codes/piv-image-gen.h
@@ -12,13 +12,12 @@ float *Z_microns, float magnification, float particle_diameter_microns, float wa
 
 
 
-void generateParticleImage(float *output_image, int num_rows, int num_cols, 
-						int num_particles, float *X, float *Y, float *particle_diameters,
-						float *particle_max_intensities, float dp_max) /* Input variables */
+int generateParticleImage(float *output_image, int num_rows, int num_cols, 
+						 int num_particles, float *X, float *Y, float *dp_pix,
+						 float *particle_max_intensities, float dp_max) /* Input variables */
 
 {
 
-	
 	// Square root of 8
 	#define sqrt8 2.82842712475
     
@@ -27,10 +26,7 @@ void generateParticleImage(float *output_image, int num_rows, int num_cols,
 	
 	// Render fraction
 	#define render_fraction 0.75
-	
-	// Particle diameters
-	#define dp particle_diameters
-	
+		
 	// Max intensities
 	#define Io particle_max_intensities
     		
@@ -40,7 +36,11 @@ void generateParticleImage(float *output_image, int num_rows, int num_cols,
 	// c is column number;
 	// ind is the pixel index 
 	// within the output array (B)
-	int p, r, c, ind; 
+	int p, r, c, ind;
+	int num_rendered = 0; 
+	
+	// Print the max particle diameter.
+	printf("dp max: %0.0f pix\n", dp_max);
 	
 	// Radial coordinates from the
 	// frame of reference of a particle
@@ -66,21 +66,21 @@ void generateParticleImage(float *output_image, int num_rows, int num_cols,
 	for(p = 0; p < num_particles; p++){		
 	
 		// Min and max rows and columns to render for that column.
-		minRenderedCol = (int)floor(X[p] - render_fraction * dp[p]);
-		maxRenderedCol = (int)ceil(X[p]  + render_fraction * dp[p]);
-		minRenderedRow = (int)floor(Y[p] - render_fraction * dp[p]);
-		maxRenderedRow = (int)ceil(Y[p]  + render_fraction * dp[p]);
+		minRenderedCol = (int)floor(X[p] - render_fraction * dp_pix[p]);
+		maxRenderedCol = (int)ceil(X[p]  + render_fraction * dp_pix[p]);
+		minRenderedRow = (int)floor(Y[p] - render_fraction * dp_pix[p]);
+		maxRenderedRow = (int)ceil(Y[p]  + render_fraction * dp_pix[p]);
     
         // Flag whether or not to render the particle
         render_particle = (minRenderedCol <= (num_cols - 1)) & (maxRenderedCol >= 0) &
                           (minRenderedRow <= (num_rows - 1)) & (maxRenderedRow >= 0) &
-					  													(dp[p] <= dp_max);
+					  													(dp_pix[p] <= dp_max);
 	
 		// Render the particle if the criteria are met.
         if(render_particle){
         
-			// Increment the number of particles rendered
-			// particles_rendered_private++;
+			// Incremement the number rendered.
+			num_rendered++;
 		
 			// Loop over all the particles.
 			for(r = minRenderedRow; r <= maxRenderedRow; r++){
@@ -92,7 +92,7 @@ void generateParticleImage(float *output_image, int num_rows, int num_cols,
 					// Flag whether or not to render the pixel
 					render_pixel = (c >= 0) & (c <= num_cols - 1) &
 								   (r >= 0) & (r <= num_rows - 1) &
-								   (render_radius <= render_fraction * dp[p]); 
+								   (render_radius <= render_fraction * dp_pix[p]); 
 				
 					// Render the pixel if the criteria are met.
 					if(render_pixel){
@@ -101,22 +101,132 @@ void generateParticleImage(float *output_image, int num_rows, int num_cols,
 						ind = r + (num_rows) * c;
 				
 						// Add the intensity to the image
-						output_image[ind] += Io[p] * dp[p] * dp[p] * PI / 32 *
+						output_image[ind] += Io[p] * dp_pix[p] * dp_pix[p] * PI / 32 *
 						                   (erf( sqrt8 * (c - X[p] - 0.5)
-						                   / dp[p]) - erf(sqrt8 *
-						                   (c - X[p] + 0.5) / dp[p])) *
+						                   / dp_pix[p]) - erf(sqrt8 *
+						                   (c - X[p] + 0.5) / dp_pix[p])) *
 						                   (erf( sqrt8 * (r - Y[p] - 0.5)
-						                   / dp[p]) - erf(sqrt8 *
-						                   (r - Y[p] + 0.5) / dp[p]));	
+						                   / dp_pix[p]) - erf(sqrt8 *
+						                   (r - Y[p] + 0.5) / dp_pix[p]));	
 						}	
 					}
 				}	
 	        }
 		}
 			
-	 			
-    return;	
+    return(particle_diameter_microns);	
 }
+
+// Generate micro PIV image.
+void generate_micro_piv_image(  float *output_image, float *X_PIX, float *Y_PIX, float *Z_microns, int num_particles,
+								int num_rows, int num_cols, float particle_diameter_microns, 
+								float pixel_size_microns, float particle_volume_fraction, 
+								float channel_depth_microns, float objective_magnification,
+								float NA, float working_distance_microns, float focal_length_microns,
+								float wavelength_microns){
+									
+	// Particle image diameters
+	float * particle_image_diameters_pixels;
+	float * particle_max_intensities;
+	
+	// Number rendered
+	int n;
+	
+	// Allocate image diameters
+	particle_image_diameters_pixels = malloc(num_particles * sizeof(float));
+	
+	// Allocate max intensities
+	particle_max_intensities = malloc(num_particles * sizeof(float));
+	
+	// Rename some variable for readability
+	float M = objective_magnification;
+	float L = wavelength_microns;
+	float dp_um = particle_diameter_microns;
+	
+	// Limit of the near-field in microns
+	// This is calculated from the description 
+	// in Eckstein & Vlachos 2009 "Digital particle image velocimetry (DPIV)
+	// robust phase correlation"
+	// by setting the size of a particle (in microns) at 
+	// a distance z from the focal plane (taken from equation 21) 
+	// equal to twice the average particle spacing (concentration in particles /
+	// / microns^3) ^(-1/3)//
+	// Particle concentration in particles per cubic micron
+	float particle_concentration = 6.0 * particle_volume_fraction / 
+	    (PI * pow(dp_um, 3));
+	
+	// Depth of field in microns
+	float depth_of_field_microns = sqrt(4.0 * pow(particle_concentration, -2.0/3.0) - 
+		    dp_um * dp_um
+				 - 5.95 * wavelength_microns * wavelength_microns / (4 * NA * NA));
+	
+	// Max particle diameter
+	float dp_max_pixels = sqrt(M * M * dp_um * dp_um + 
+		pow((2.44 * (M + 1) * L) / (2 * NA), 2) + 
+		pow(2 * NA * M * depth_of_field_microns, 2)) / pixel_size_microns;
+	
+	printf("Particle concentration: %0.2f particles per cubic micron\n", particle_concentration);
+	
+	// Calculate the particle image diameters and intensities								
+	calculate_particle_image_size(particle_image_diameters_pixels, particle_max_intensities, // Result arrays
+			Z_microns, objective_magnification, dp_um, 
+			wavelength_microns, NA, pixel_size_microns, num_particles);
+			
+	// Generate the image
+	n = generateParticleImage(output_image, num_rows, num_cols, 
+							num_particles, X_PIX, Y_PIX, particle_image_diameters_pixels,
+							particle_max_intensities, dp_max_pixels); /* Input variables */
+							
+	printf("%d particles rendered.\n", n);
+	
+	
+	// Free memory		
+	free(particle_image_diameters_pixels);	
+	free(particle_max_intensities);						
+}
+
+// Calculate particle image diameter in diffraction limited optics.
+// Microscope model taken from Olsen // Adrian 2000
+void calculate_particle_image_size(float *particle_image_diameters_pixels, float *particle_max_intensities, // Result arrays
+float *Z_microns, float magnification, float particle_diameter_microns, float wavelength_microns, float NA, float pixel_size_microns, int num_particles){
+	
+	// Redefine some variables for readability
+	float M2 = magnification * magnification;
+	float dp2 = particle_diameter_microns * particle_diameter_microns;
+	float NA2 = NA * NA;
+	float L = wavelength_microns;
+	float particle_image_diameter_microns;
+	
+	// Counter
+	int n;
+							  	
+	// Point response diameter
+	float ds = 2.44 * (magnification + 1) * L / (2 * NA);
+	float ds2 = ds * ds;
+	
+	printf("Point response diameter: %0.2f microns\n", ds);
+  									
+	// In focus diameter
+	float de_focused = sqrt(M2 * dp2 + ds2);
+	
+	printf("In-focus diameter: %0.2f microns\n", de_focused);
+
+	/*
+	% Image diameter
+	particle_image_diameter_microns =...
+	    sqrt(M^2 * dp .^2 + 5.95 * (M + 1)^2 * L^2 /  (4 * NA^2) + ...
+	   (2 * NA * M * Z_MICRONS).^2);
+	*/
+
+	// Image diameters in microns
+	// and max intensities.
+	for(n = 0; n < num_particles; n++){
+		particle_image_diameter_microns = sqrt( M2 * dp2 + ds2 + 4 * NA2 * M2 * Z_microns[n] * Z_microns[n]);
+		particle_image_diameters_pixels[n] = particle_image_diameter_microns / pixel_size_microns;
+		particle_max_intensities[n] = de_focused * de_focused / (particle_image_diameter_microns * particle_image_diameter_microns);	
+	}								
+}
+
 
 // Random number generator.
 void devrand(float *array, int array_length, float lower_bound, float upper_bound) {
@@ -166,85 +276,8 @@ int seed_random_number_generator(){
 	return(0);
 }
 
-// Generate micro PIV image.
-void generate_micro_piv_image( float *output_image, float *X_PIX, float *Y_PIX, float *Z_microns, int num_particles,
-								int num_rows, int num_cols, float particle_diameter_microns, 
-								float pixel_size_microns, float particle_volume_fraction, 
-								float channel_depth_microns, float objective_magnification,
-								float NA, float working_distance_microns, float focal_length_microns,
-								float wavelength_microns){
-									
-	// Particle image diameters
-	float * particle_image_diameters_pixels;
-	float * particle_max_intensities;
-	
-	// Allocate image diameters
-	particle_image_diameters_pixels = malloc(num_particles * sizeof(float));
-	
-	// Allocate max intensities
-	particle_max_intensities = malloc(num_particles * sizeof(float));
-	
-	
-	// Limit of the near-field in microns
-	// This is calculated from the description 
-	// in Eckstein & Vlachos 2009 "Digital particle image velocimetry (DPIV)
-	// robust phase correlation"
-	// by setting the size of a particle (in microns) at 
-	// a distance z from the focal plane (taken from equation 21) 
-	// equal to twice the average particle spacing (concentration in particles /
-	// / microns^3) ^(-1/3)//
 
-	// Particle concentration in particles per cubic micron
-	float particle_concentration = 6.0 * particle_volume_fraction / 
-	    (PI * pow(particle_diameter_microns, 3));
-	
-	// Largest diameter of a particle that is considered "in focus," in pixels.
-	float dp_max_pixels = pow(particle_concentration, -1.0/3.0) * objective_magnification / pixel_size_microns;
-	
-	// Calculate the particle image diameters and intensities								
-	calculate_particle_image_size(particle_image_diameters_pixels, particle_max_intensities, // Result arrays
-			Z_microns, objective_magnification, particle_diameter_microns, 
-			wavelength_microns, NA, pixel_size_microns, num_particles);
-			
-	// Generate the image
-	generateParticleImage(output_image, num_rows, num_cols, 
-							num_particles, X_PIX, Y_PIX, particle_image_diameters_pixels,
-							particle_max_intensities, dp_max_pixels); /* Input variables */
-			
-	free(particle_image_diameters_pixels);	
-	free(particle_max_intensities);						
-}
 
-// Calculate particle image diameter in diffraction limited optics.
-// Microscope model taken from Olsen // Adrian 2000
-void calculate_particle_image_size(float *particle_image_diameters_pixels, float *particle_max_intensities, // Result arrays
-float *Z_microns, float magnification, float particle_diameter_microns, float wavelength_microns, float NA, float pixel_size_microns, int num_particles){
-	
-	// Redefine some variables for readability
-	float M2 = magnification * magnification;
-	float dp2 = particle_diameter_microns * particle_diameter_microns;
-	float NA2 = NA * NA;
-	float L = wavelength_microns;
-	float particle_image_diameter_microns;
-	
-	// Counter
-	int n;
-							  	
-	// Point response diameter
-	float ds = 2.44 * (magnification + 1) * L / (2 * NA);
-	float ds2 = ds * ds;
-  									
-	// In focus diameter
-	float de_focused = sqrt(M2 * dp2 + ds2);
-
-	// Image diameters in microns
-	// and max intensities.
-	for(n = 0; n < num_particles; n++){
-		particle_image_diameter_microns = sqrt( M2 * dp2 + ds2 + 4 * NA2 * M2 * Z_microns[n] * Z_microns[n]);
-		particle_image_diameters_pixels[n] = particle_image_diameter_microns * magnification / pixel_size_microns;
-		particle_max_intensities[n] = de_focused * de_focused / (particle_image_diameter_microns * particle_image_diameter_microns);	
-	}								
-}
 
 
 
