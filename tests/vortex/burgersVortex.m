29d789023ac54a20105e567b402dad09655bb88e
Matthew Giarra <matthew.giarra@jhuapl.edu>
2019-09-16 23:22:14 -0400

    Consolidate burgersVortex functions

--- tests/vortex/burgersVortex.m
+++ tests/vortex/burgersVortex.m
@@ -1,4 +1,4 @@
-function [X, Y, Z, T] = burgersVortex(X0, Y0, Z0, TIMESPAN);
+function [X, Y, Z, T] = burgersVortex(X0, Y0, Z0, TIMESPAN)
 % [X, Y, T] = lambOseenVortexRing(X0, Y0, TIMESPAN, VORTEXPARAMETERS)
 % This function advects massless flow tracers with initial positions [X0, Y0]
 % over a time-vector specified by TIMESPAN according to a Lamb-Oseen vortex
@@ -154,6 +154,41 @@ end
 
 end
 
+
+
+function U = burgersVortexVelocityFunction(T, X, PARAMS)
+
+% X comes in as [x; y; z] where x, y, and z
+% are column vectors of length nPoints
+
+g = PARAMS.g;
+a = PARAMS.a;
+m = PARAMS.m;
+
+
+
+nPoints = numel(X) / 3;
+x = X(1 : nPoints);
+y = X(nPoints + 1 : 2*nPoints);
+z = X(2*nPoints + 1 : end);
+[th, r, z] = cart2pol(x,y,z);
+
+ur = -1/2 * a * r;
+uth = g ./ (2 * pi * r) .* (1 - exp(-(a*r.^2) / (4 * m)));
+uz = a * z;
+
+ux = ur .* cos(th) - r .* uth .* sin(th);
+uy = ur .* sin(th) + r .* uth .* cos(th);
+
+U = [ux; uy; uz];
+
+% f = @(t,x) [(-1/2)*A*r; 
+%             (G/(2*pi*r))*(1-exp(-(A*r^2)/(4*m)));
+%             A*z];
+        
+        
+        
+end
 
 
 
